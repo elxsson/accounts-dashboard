3c4e4a2f7cb0ff6585770bb7e091efb5dc2bec1f
soullcake <soullcakezz@gmail.com>
2024-08-20 16:24:45 -0300

    feat: adiciona function operation

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import inquirer from "inquirer"
 import chalk from "chalk"
 import fs from 'fs'
 
+const operation = ()=> {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'actions',
+        message: 'O que voce deseja fazer?',
+        choices: [
+            'Criar conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }]).then((answer) => {
+        
+        const action = answer['action']
+        console.log(action)
+
+    }).catch(err => console.log(err))
+}
+
+operation()
